c0c02bd46681940199e6a70a6152d24c062588a4
Brenthore <franzisbugna@yahoo.com>
2019-09-22 07:38:36 +0800

    Updates

---
+++ precache-manifest.e94eb6997b77d4401f3e752d3599b949.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c80be21cde9aa8adbe0416c4de1fbc9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4f77eb8a488302552881",
+    "url": "/monster-rolodex/static/css/main.53e9f43a.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monster-rolodex/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "4f77eb8a488302552881",
+    "url": "/monster-rolodex/static/js/main.ef37c898.chunk.js"
+  },
+  {
+    "revision": "eac7e1388a413f076798",
+    "url": "/monster-rolodex/static/js/runtime~main.f2a7c9f4.js"
+  }
+]);
